1f3acbb62939b4a609d5ccdf54ff0bcb9e5c9aa9
Joao <jofisaes@gmail.com>
2016-04-03 10:33:44 +0200

    JFSE-01 Adds webapp files using archetypes

---
+++ Readme.md
@@ -0,0 +1,4 @@
+## Stones game
+
+# Creation commands
+mvn archetype:generate -DgroupId=jfse -DartifactId=stones-game -DarchetypeArtifactId=maven-archetype-webapp -DinteractiveMode=false
--- src/main/java/com/jfse/stonesgame/manager/BoardManager.java
+++ src/main/java/com/jfse/stonesgame/manager/BoardManager.java
@@ -1,12 +1,19 @@
 package com.jfse.stonesgame.manager;
 
+import com.jfse.stonesgame.objects.Board;
+import com.jfse.stonesgame.objects.BoardImpl;
+import com.jfse.stonesgame.objects.Player;
+
 /**
  * Created by joaofilipesabinoesperancinha on 02-04-16.
  */
 public class BoardManager {
 
-    BoardManager(int numberOfPits)
+    private final Board board;
+
+    BoardManager(int nPits, int nInitialStones, Player player1, Player player2)
     {
 
+        this.board = new BoardImpl(nPits, nInitialStones,player1,player2);
     }
 }
