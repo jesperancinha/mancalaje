d222eec92ef469a316629a6a9159552d2c27e690
Joao <jofisaes@hotmail.com>
2016-07-11 23:43:04 +0200

    SZ-N Grunt default registered task

--- gruntfile.js
+++ gruntfile.js
@@ -2,6 +2,15 @@ module.exports = function(grunt){
 	grunt.initConfig({
 
         pkg: grunt.file.readJSON('package.json'),
+        cssmin: {
+             options: {
+             }, target: {
+                    files: {
+                        'output.css' : [ 'style.css', 'reset.css']
+                    }
+             }
+        }
    });
    grunt.loadNpmTasks('grunt-contrib-cssmin');
+   grunt.registerTask('default', ['cssmin'])
 };
