1bfd7f3b4f128203f0a704c994c769bb1ad6e6fa
Joao Esperancinha <jofisaes@gmail.com>
2019-07-15 18:19:35 +0200

    QA - Unit tests improvements

--- mancalaje-service/src/test/java/com/jofisaes/mancala/MancalaApplicationTests.java
+++ mancalaje-service/src/test/java/com/jofisaes/mancala/MancalaApplicationTests.java
@@ -1,16 +1,24 @@
 package com.jofisaes.mancala;
 
+import com.jofisaes.mancala.services.UserManagerService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MancalaApplicationTests {
 
+    @Autowired
+    private UserManagerService userManagerService;
+
     @Test
     public void contextLoads() {
+        assertThat(userManagerService).isNotNull();
     }
 
 }
