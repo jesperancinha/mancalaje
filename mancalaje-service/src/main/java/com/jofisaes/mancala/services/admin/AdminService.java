21fd6a57e31d53a6fc592d56d03c68eac4f72040
Joao Esperancinha <jofisaes@gmail.com>
2019-07-29 22:13:11 +0200

    Resets refresh reat to 5 minutes

--- mancalaje-service/src/main/java/com/jofisaes/mancala/services/admin/AdminService.java
+++ mancalaje-service/src/main/java/com/jofisaes/mancala/services/admin/AdminService.java
@@ -35,7 +35,7 @@ public class AdminService {
         this.session = session;
     }
 
-    @Scheduled(cron = "0 */1 * ? * *")
+    @Scheduled(cron = "0 */5 * ? * *")
     public void removeExpiredUsers() throws Exception {
         MessageProducer producer = session.createProducer(session.createTopic("UserSweepConsumerTopic"));
         String payload = "User Sweep";
