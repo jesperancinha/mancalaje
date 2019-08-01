1fb4c33a128d7cfc0269ea1c1bdf6c778a84a899
Joao Esperancinha <jofisaes@gmail.com>
2019-08-01 20:31:14 +0200

    QA - Code cleanup

--- mancalaje-fe/src/actions/Validators.tsx
+++ mancalaje-fe/src/actions/Validators.tsx
@@ -1,11 +1,13 @@
 const MIN_TEXT_LENGTH = 0;
 
+const regExp = new RegExp("^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$");
+
 const invalidateText = (text: string) => {
     return text.length === MIN_TEXT_LENGTH;
 };
 
 const invalidateEmail = (text: string) => {
-    return !/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(text);
+    return !regExp.test(text);
 };
 
 export {invalidateText, invalidateEmail, MIN_TEXT_LENGTH};
