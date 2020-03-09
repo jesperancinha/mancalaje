e4b2725dbeacc663106677cec18f8b75ff18abd5
Joao Esperancinha <jofisaes@gmail.com>
2020-03-09 20:22:59 +0100

    removes unnecessary exposures

--- createDeploymentRunServices.sh
+++ createDeploymentRunServices.sh
@@ -3,6 +3,4 @@ kubectl create -f docker-psql/postgres-deployment.yaml
 kubectl create -f mancalaje-service/mancalaje-deployment.yaml
 kubectl create -f mancalaje-fe/mancalaje-fe-deployment.yaml
 
-minikube service mancalaje-postgresql
-minikube service mancalaje
 minikube service mancalaje-fe
