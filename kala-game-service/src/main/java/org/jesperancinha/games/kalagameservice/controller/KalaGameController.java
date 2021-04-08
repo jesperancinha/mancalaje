169baee0064e80331920ac25543a0abdbf7127c2
Joao Esperancinha <jofisaes@gmail.com>
2021-04-08 21:15:12 +0200

    Moves possible

--- kala-game-service/src/main/java/org/jesperancinha/games/kalagameservice/controller/KalaGameController.java
+++ kala-game-service/src/main/java/org/jesperancinha/games/kalagameservice/controller/KalaGameController.java
@@ -2,6 +2,8 @@ package org.jesperancinha.games.kalagameservice.controller;
 
 import org.jesperancinha.games.kalagameservice.dto.BoardDto;
 import org.jesperancinha.games.kalagameservice.dto.converters.BoardConverter;
+import org.jesperancinha.games.kalagameservice.model.Board;
+import org.jesperancinha.games.kalagameservice.model.Pit;
 import org.jesperancinha.games.kalagameservice.model.Player;
 import org.jesperancinha.games.kalagameservice.service.BoardService;
 import org.jesperancinha.games.kalagameservice.service.GameService;
@@ -9,7 +11,7 @@ import org.jesperancinha.games.kalagameservice.service.PlayerService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,11 +47,25 @@ public class KalaGameController {
         return BoardConverter.toDto(newBoard);
     }
 
+    @PutMapping("move/{boardId}/{pitId}")
+    public BoardDto move(Principal principal,
+                         @PathVariable
+                                 Long boardId,
+                         @PathVariable
+                                 Long pitId) {
+        var player = playerService.createOrFindPlayerByName(principal.getName());
+        final Board board = boardService.findBoardById(boardId);
+        final Pit startPit = board.getPits().stream().filter(pit -> pit.getId().equals(pitId)).findAny().orElse(null);
+        final Board boardUpdated = gameService.sowStonesFromPit(player, startPit, board);
+        return BoardConverter.toDto(boardUpdated);
+    }
+
     @GetMapping
     public List<BoardDto> getAllBoardsPerPlayer(Principal principal) {
         final Player player = playerService.createOrFindPlayerByName(principal.getName());
         return boardService.findBoardsByPlayer(player);
     }
+
     @GetMapping("available")
     public List<BoardDto> getAvailableBoards() {
         return boardService.findAvailableBoards();
