c78247880c6ac3e21da630fa2c0b7f9865efdf07
Joao Esperancinha <jofisaes@gmail.com>
2021-05-09 19:20:37 +0200

    🔘 CircleCI Quality

--- kala-game-web/src/app/services/player.service.ts
+++ kala-game-web/src/app/services/player.service.ts
@@ -3,23 +3,22 @@ import {HttpClient} from "@angular/common/http";
 import {Observable, of} from "rxjs";
 import {catchError, retry} from "rxjs/operators";
 
-
-const fetchBoardUSer = "/log/user"
+const fetchBoardUSer = "/log/user";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class PlayerService {
   constructor(private http: HttpClient) {
   }
 
   public getLoggedUser() {
-    return this.http.get(fetchBoardUSer, {responseType: 'text'})
+    return this.http.get(fetchBoardUSer, {responseType: "text"})
       .pipe(retry(3), catchError(this.handleError<string>()));
 
   }
 
-  handleError<T>(operation = 'operation', result?: T) {
+  handleError<T>(operation = "operation", result?: T) {
     return (error: any): Observable<T> => {
       console.error(error);
       console.log(`${operation} failed: ${error.message}`);
