62b3d5843672fe1bd560a65d0834677097ac0e7d
Joao Esperancinha <jofisaes@gmail.com>
2021-05-10 11:34:58 +0200

    🗿 Merging coveralls report

--- packages/mancalaje-fe/src/actions/Refresher.tsx
+++ packages/mancalaje-fe/src/actions/Refresher.tsx
@@ -1,9 +1,9 @@
 const removeAllRefreshers = () => {
     const intervalId = setInterval(() => {
     }, 1);
-    for (let i = 1; i < intervalId; i++) {
-        clearInterval(i);
-    }
+    // for (let i = 1; i < intervalId; i++) {
+    //     clearInterval(i);
+    // }
 };
 
 export {removeAllRefreshers};
