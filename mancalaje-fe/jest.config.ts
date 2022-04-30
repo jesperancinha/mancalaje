395e1162425fdaa088d385182b8a28271872ef7e
Joao Esperancinha <jofisaes@gmail.com>
2022-04-30 20:42:24 +0200

    🌿🇺🇦🕊 Back to square one on Mancala React TS application

---
+++ mancalaje-fe/jest.config.ts
@@ -0,0 +1,18 @@
+import type {Config} from '@jest/types';
+// Sync object
+const config: Config.InitialOptions = {
+    verbose: true,
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+        "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
+    },
+    testEnvironment: "jsdom",
+    setupFilesAfterEnv: [
+        "<rootDir>/setupTests.ts"
+    ]
+
+};
+export default config;
